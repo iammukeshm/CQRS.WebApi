047a0b7edfb2c975f5fa45902a05f28796a9ada6
Mukesh Murugan <iammukeshm@gmail.com>
2020-05-16 00:04:01 +0530

    Add project files.

---
+++ CQRS.WebApi/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRS.WebApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string Description { get; set; }
+        public decimal Rate { get; set; }
+        public decimal BuyingPrice { get; set; }
+        public string ConfidentialData { get; set; }
+    }
+}
